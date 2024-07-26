7e1c416068ca704eea38ed1fea84fe064b170d45
wsxiaolin <xiegushi2022@outlook.com>
2024-07-26 20:19:13 +0800

    一些小更新

---
+++ test.js
@@ -0,0 +1,30 @@
+const sqlite3 = require("sqlite3").verbose();
+
+// 创建一个新的数据库连接
+let db = new sqlite3.Database("./discussion.db", (err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
+
+function queryData(tag, name) {
+  const sqlQuery = "SELECT * FROM data WHERE type = ? AND name LIKE ? LIMIT 10";
+
+  db.all(sqlQuery, [tag, `%${name}%`], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      console.log(row);
+    });
+  });
+}
+
+// 使用示例
+queryData("精选", "核");
+
+db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+});
---
+++ update.js
@@ -0,0 +1,50 @@
+const { User } = require("physics-lab-web-api");
+const sqlite3 = require("sqlite3").verbose();
+const update = ["精选", "知识库"];
+
+const pl = new User();
+const db = new sqlite3.Database("discussion.db");
+
+function insertData(id, name, tag) {
+  db.get(`SELECT id FROM data WHERE id = ?`, [id], (err, row) => {
+    if (err) {
+      console.error("查询数据出错:", err.message);
+    } else if (row) {
+      console.error(`ID ${id} 已存在`);
+    } else {
+      db.run(
+        `INSERT INTO data (id, name, type) VALUES (?, ?, ?)`,
+        [id, name, tag],
+        function (err) {
+          if (err) {
+            console.error("插入数据出错:", err.message);
+          } else {
+            console.log(`插入数据成功：ID ${id}, Name ${name}, Type ${tag}`);
+          }
+        }
+      );
+    }
+  });
+}
+
+async function get(tag) {
+  const re = await pl.projects.query("Discussion", {
+    tags: [tag],
+    take: 10,
+  });
+
+  for (const i of re.Data.$values) {
+    await insertData(i.ID, i.Subject, tag);
+  }
+}
+async function main() {
+  await pl.auth.login();
+
+  for (const tag of update) {
+    await get(tag).then(() => {
+      console.log(tag);
+    });
+  }
+}
+
+main();
